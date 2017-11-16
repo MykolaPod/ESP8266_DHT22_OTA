281157b395dca40a9a86b7a1de63cb10a5988dc8
MykolaPo <88.general@gmail.com>
2017-11-16 16:10:21 +0200

    added libs to work with wifi

--- esp8266anddht22/esp8266anddht22.ino
+++ esp8266anddht22/esp8266anddht22.ino
@@ -1,46 +1,73 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
 #include <DHT.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT22
 
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("DHTxx test!");
+  
 
+   bool connectionResult = tryConnectToWifi("eGO-WiFi","1111111111111");
+   if(!connectionResult){
+    //ESP.restart();
+    Serial.println("Wifi not connected !");
+   }
+   else {
+      ArduinoOTA.setHostname("Ego-ESP8266-tempSensor");
+      ArduinoOTA.begin();      
+   } 
+   
   dht.begin();
 }
 
+ 
 void loop() {
- 
-  delay(10000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
- 
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
+  ArduinoOTA.handle();
+  
+  // Reading temperature or humidity takes about 250 milliseconds! 
+  float h = dht.readHumidity();  
   float t = dht.readTemperature();
   
-
-  // Check if any reads failed and exit early (to try again).
+  delay(10000);
+  
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+  
   Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print(" %\t");
   Serial.print("Temperature: ");
   Serial.print(t);
-  Serial.print(" *C ");
-  
+  Serial.print(" *C ");  
+  Serial.println("");
 }
+
+  bool tryConnectToWifi(char* ssid, char* password) {
+      bool isConnected = false;
+    
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
+      delay(15000);
+    
+     if( WiFi.waitForConnectResult() == WL_CONNECTED){
+      return true;
+     }
+     else{
+      return false;
+     }
+    
+  }
+
+
+ 
+  
+
+ 
+
